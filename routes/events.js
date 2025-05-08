c68dab9f6be59536dd6a5470a7751f6d66ab4433
Dannie Vinther <davi@kea.dk>
2025-05-08 13:31:26 +0200

    Add event seeding functionality and dummy data for testing

--- routes/events.js
+++ routes/events.js
@@ -4,6 +4,7 @@ const eventsController = require("../controllers/eventsController");
 
 router.get("/", eventsController.getEvents);
 router.post("/", eventsController.createEvent);
+router.post("/seed", eventsController.seedEvents);
 router.get("/:id", eventsController.getEventById);
 router.patch("/:id", eventsController.updateEvent);
 router.delete("/:id", eventsController.deleteEvent);
